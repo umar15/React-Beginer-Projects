7b7e13eef343394640b3c715c2fce1aa1859e87e
Umar Khalid <umar.qur105@gmail.com>
2020-11-26 18:38:39 +0500

    Tours Project Added

--- src/Projects/Tours/AllTours.js
+++ src/Projects/Tours/AllTours.js
@@ -1,8 +1,22 @@
 import React from "react";
 import SingleTour from "./SingleTour";
 
-const Tours = () => {
-	return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+	return (
+		<section>
+			<div className="title">
+				<h2>Our Tours</h2>
+				<div className="underline"></div>
+			</div>
+			<div>
+				{tours.map((tour) => {
+					return (
+						<SingleTour key={tour.id} {...tour} removeTour={removeTour} />
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Tours;
--- src/Projects/Tours/Tours.js
+++ src/Projects/Tours/Tours.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AllTours from "./AllTours";
 import Loading from "./Loading";
+import "./Tours.css";
 
 const url = "https://course-api.com/react-tours-project";
 
 const Tours = () => {
 	const [loading, setLoading] = useState(true);
 	const [tours, setTours] = useState([]);
+
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
 
 	if (loading)
 		return (
@@ -15,9 +38,22 @@ const Tours = () => {
 			</main>
 		);
 
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className="title">
+					<h2>No tours left</h2>
+				</div>
+				<button onClick={fetchTours} className="btn">
+					refresh
+				</button>
+			</main>
+		);
+	}
+
 	return (
 		<main>
-			<AllTours />
+			<AllTours tours={tours} removeTour={removeTour} />
 		</main>
 	);
 };
